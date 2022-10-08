bdb1d7f2f0dfa091e5773bc5ce8809d3442fe261
wu147147147 <114759026+wu147147147@users.noreply.github.com>
2022-10-08 13:05:28 +0800

    Create 輸出.c

---
+++ 輸出.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int i,j; //i 列表的最後一位,j為新變數
+
+for(j = 0,a[i]!=0,i--){
+        printf("%c",a[i]);
+        j+1;
+        if (j/3 == 0){
+            printf(",");
+        }
+        
+
+}
+
+
+return 0;
+}
